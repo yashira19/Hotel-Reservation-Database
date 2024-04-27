d04ad53fd4d61cbfae7c9e7aacc7017c336ce2af
yashira19 <79137470+yashira19@users.noreply.github.com>
2024-04-27 12:42:50 -0700

    this is the home page
    home_page.php is where the user after successfully log in, it shows the option that the user can do. I already linked the book link to the "book.php" and the log out link to the "logout.php". the view and cancel needs to be linked w the pages.

---
+++ components/home_page.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); 
+
+if (!isset($_SESSION["firstName"])) {
+    header("Location: login.php"); 
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home Page</title>
+</head>
+<body>
+  <h1>Welcome to the Hotel Reservation System</h1>
+  <p>Please select an option:</p>
+  <ul>
+    <li><a href="book.php">Book a Room</a></li>
+    <li><a href="view_reservations.php">View Reservations</a></li>
+    <li><a href="cancel_reservation.php">Cancel Reservation</a></li>
+    <li><a href="../components/index.php">Log out</a></li>
+
+  </ul>
+</body>
+</html>
